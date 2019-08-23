f656cc6d607574e282ec4ae2b69a3372deb8521f
AugustoMarcelo <mrclgst10@gmail.com>
2019-08-22 22:12:43 -0300

    Salvando post no banco

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,9 +2,19 @@ const routes = require('express').Router();
 const multer = require('multer');
 const multerConfig = require('./config/multer');
 
-routes.post('/posts', multer(multerConfig).single('file'), (request, response) => {
-  console.log(request.file);
-  response.json({ hello: 'world' });
+const Post = require('./models/Post');
+
+routes.post('/posts', multer(multerConfig).single('file'), async (request, response) => {
+  const { originalname: name, size, filename: key } = request.file;
+
+  const post = await Post.create({
+    name,
+    size,
+    key,
+    url: '',
+  });
+
+  response.json(post);
 });
 
 module.exports = routes;
