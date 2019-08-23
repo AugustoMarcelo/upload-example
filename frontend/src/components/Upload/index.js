2b0f459f2ff367f2be072e0c0a76fa674f5e07af
AugustoMarcelo <mrclgst10@gmail.com>
2019-08-23 19:32:02 -0300

    Estruturando e estilizando componente de upload

--- frontend/src/components/Upload/index.js
+++ frontend/src/components/Upload/index.js
@@ -1,5 +1,30 @@
 import React from 'react';
 
+import Dropzone from 'react-dropzone';
+
+import { DropContainer, UploadMessage } from './styles';
+
 export default function Upload() {
-  return <div className=""/>;
+  function renderDragMessage(isDragActive, isDragReject) {
+    if (!isDragActive) {
+      return <UploadMessage>Arraste arquivos aqui...</UploadMessage>
+    }
+
+    if (isDragReject) {
+      return <UploadMessage type="error">Arquivo não suportado</UploadMessage>
+    }
+
+    return <UploadMessage type="success">Solte os arquivos aqui</UploadMessage>
+  }
+
+  return (
+    <Dropzone accept="image/*" onDropAccepted={() => {}}>
+      { ({ getRootProps, getInputProps, isDragActive, isDragReject }) => (
+        <DropContainer {...getRootProps()} isDragActive={isDragActive} isDragReject={isDragReject}>
+          <input {...getInputProps()} />
+          {renderDragMessage(isDragActive, isDragReject)}
+        </DropContainer>
+      ) }
+    </Dropzone>
+  );
 }
