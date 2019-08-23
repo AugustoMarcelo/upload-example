1ffa50a6c922d754faaf9267ea4a97213358e85c
AugustoMarcelo <mrclgst10@gmail.com>
2019-08-23 08:58:57 -0300

    Utilização dos midlewares do mongoose para gerar a url de acesso ao arquivo e remoção física do mesmo

--- backend/src/models/Post.js
+++ backend/src/models/Post.js
@@ -1,4 +1,10 @@
 const { Schema, model } = require('mongoose');
+const aws = require('aws-sdk');
+const fs = require('fs');
+const path = require('path');
+const { promisify } = require('util');
+
+const s3 = new aws.S3();
 
 const PostSchema = new Schema({
   name: String,
@@ -8,6 +14,27 @@ const PostSchema = new Schema({
   createdAt: {
     type: Date,
     default: Date.now,
+  }
+});
+
+/**
+ * Utilizando middleware do mongoose para realizar uma operação antes de outra.
+ * Não foi utilizado arrow functions devido a não possibilidade de utilizar o _this_ para referenciar o atributo url.
+ */
+PostSchema.pre('save', function() {
+  if (!this.url) {
+    this.url = `${process.env.APP_URL}/files/${this.key}`;
+  }
+});
+
+PostSchema.pre('remove', function() {
+  if (process.env.STORAGE_TYPE === 's3') {
+    return s3.deleteObject({
+      Bucket: process.env.AWS_BUCKET_NAME,
+      Key: this.key,
+    }).promise();
+  } else {
+    return promisify(fs.unlink)(path.resolve(__dirname, '..', '..', 'tmp', 'uploads', this.key));
   }
 });
 
