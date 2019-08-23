def2096b8af008e418008cb487c5b6cb9be8e965
AugustoMarcelo <mrclgst10@gmail.com>
2019-08-23 20:14:07 -0300

    Importando FileList

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,14 @@ import GlobalStyle from './styles/global';
 import { Container, Content } from './styles';
 
 import Upload from './components/Upload';
+import FileList from './components/FileList';
 
 function App() {
   return (
     <Container>
       <Content>
         <Upload />
+        <FileList />
       </Content>
       <GlobalStyle />
     </Container>
