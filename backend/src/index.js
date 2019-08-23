7d6e7fdf9fbd51945f2cb984b174318cf3e2895e
AugustoMarcelo <mrclgst10@gmail.com>
2019-08-23 09:00:22 -0300

    Utilizando middleware do express para gerar uma rota estática e permitir acesso aos arquivos via url

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
+
 require('./config/database');
 
 const app = express();
@@ -9,6 +11,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
+// Rota que provê acesso aos uploads locais, através da url /files/:filename
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')));
 
 app.use(require('./routes'));
 
