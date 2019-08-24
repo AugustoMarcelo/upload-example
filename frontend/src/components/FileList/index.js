3fb07323228df4021a93dd104ec587e91aedc896
AugustoMarcelo <mrclgst10@gmail.com>
2019-08-24 16:37:59 -0300

    Preenchendo lista com os arquivos

--- frontend/src/components/FileList/index.js
+++ frontend/src/components/FileList/index.js
@@ -4,40 +4,49 @@ import { MdCheckCircle, MdError, MdLink } from 'react-icons/md';
 
 import { Container, FileInfo, Preview } from './styles';
 
-export default function FileList() {
+export default function FileList({ files, onDelete }) {
   return (
     <Container>
-      <li>
-        <FileInfo>
-          <Preview src="https://uploadexampleclass.s3.amazonaws.com/ca183e4c0787730e2e9121f7022b5375-ccxp.jpg" />
+      {!!files && files.map(uploadedFile => (
+        <li key={uploadedFile.id}>
+          <FileInfo>
+            <Preview src={uploadedFile.preview} />
+            <div>
+              <strong>{uploadedFile.name}</strong>
+              <span>
+                {uploadedFile.readableSize}
+                {!!uploadedFile.url && (<button onClick={() => onDelete(uploadedFile.id)}>Excluir</button>)}
+              </span>
+            </div>
+          </FileInfo>
+
           <div>
-            <strong>profile.png</strong>
-            <span>64kb <button onClick={() => {}}>Excluir</button></span>
+            {!uploadedFile.uploaded && !uploadedFile.error && (
+              <CircularProgressbar
+                styles={{
+                  root: { width: 24 },
+                  path: { stroke: '#7159c1' }
+                }}
+                strokeWidth={10}
+                value={uploadedFile.progress}
+              />
+            )}
+
+            {uploadedFile.url && (
+              <a 
+                href={uploadedFile.url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <MdLink style={{ marginRight: 8 }} size={24} color="#222" />
+              </a>
+            )}
+
+            { uploadedFile.uploaded && <MdCheckCircle size={24} color="#78e5d5" /> }
+            { uploadedFile.error && <MdError size={24} color="#e57878" /> }
           </div>
-        </FileInfo>
-
-        <div>
-          <CircularProgressbar
-            styles={{
-              root: { width: 24 },
-              path: { stroke: '#7159c1' }
-            }}
-            strokeWidth={10}
-            value={60}
-          />
-
-          <a 
-            href="https://uploadexampleclass.s3.amazonaws.com/ca183e4c0787730e2e9121f7022b5375-ccxp.jpg"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <MdLink style={{ marginRight: 8 }} size={24} color="#222" />
-          </a>
-
-          <MdCheckCircle size={24} color="#78e5d5" />
-          <MdError size={24} color="#e57878" />
-        </div>
-      </li>
+        </li>
+      ))}
     </Container>
   );
 }
