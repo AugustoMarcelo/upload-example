771f4731f068efc8a58f9470a193461aa6303cdf
AugustoMarcelo <mrclgst10@gmail.com>
2019-08-24 16:34:35 -0300

    Adicionando CORS

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,11 +3,13 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const cors = require('cors');
 
 require('./config/database');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
